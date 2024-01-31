01118371dfccce420dfe4a23af05464695f55df9
Gavin <gavinwacira@gmail.com>
2024-01-31 22:49:33 +0300

    javascript

---
+++ index.js
@@ -0,0 +1,57 @@
+
+console.log("Hello, World!");
+
+
+// Declare variables 
+let num = 10;
+let str = "JavaScript";
+let bool = true;
+console.log(num, str, bool);
+
+
+// functions
+function addNumbers(a, b) {
+    return a + b;
+}
+console.log(addNumbers(5, 3));
+
+// array 
+let numbers = [1, 2, 3, 4, 5];
+
+for (let num of numbers) {
+    console.log(num);
+}
+
+
+// Create objects
+let person = {
+    name: "John",
+    age: 25,
+};
+console.log(person.name, person.age);
+
+
+// conditions
+let number = 0;
+
+if (number > 0) {
+    console.log("Positive");
+} else if (number < 0) {
+    console.log("Negative");
+} else {
+    console.log("Zero");
+}
+
+
+// for loop 
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+
+
+
+
+
+
+
