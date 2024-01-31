a40e13e59c944b26e481f470d004cf1909edf4c8
Gavin <gavinwacira@gmail.com>
2024-01-31 22:57:30 +0300

    typescript two

--- index.js
+++ index.js
@@ -1,57 +1,78 @@
-
-console.log("Hello, World!");
-
-
-// Declare variables 
-let num = 10;
-let str = "JavaScript";
-let bool = true;
-console.log(num, str, bool);
-
-
-// functions
-function addNumbers(a, b) {
-    return a + b;
-}
-console.log(addNumbers(5, 3));
-
-// array 
-let numbers = [1, 2, 3, 4, 5];
-
-for (let num of numbers) {
-    console.log(num);
-}
-
-
-// Create objects
-let person = {
-    name: "John",
-    age: 25,
-};
-console.log(person.name, person.age);
-
-
-// conditions
-let number = 0;
-
-if (number > 0) {
-    console.log("Positive");
-} else if (number < 0) {
-    console.log("Negative");
-} else {
-    console.log("Zero");
-}
-
-
-// for loop 
-for (let i = 1; i <= 5; i++) {
-    console.log(i);
-}
+// asynchnonous
+function fetchData() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const data = { message: "Data fetched successfully!" };
+        resolve(data);
+      }, 2000);
+    });
+  }
+  
+  fetchData()
+    .then((result) => {
+      console.log(result.message);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
 
 
+    // Using Arrow Functions and Array Methods
+const numbers = [1, 2, 3, 4, 5];
+
+const doubledNumbers = numbers.map((num) => num * 2);
+
+const evenNumbers = numbers.filter((num) => num % 2 === 0);
+
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+console.log(doubledNumbers, evenNumbers, sum);
 
 
+// Class and Inheritance
+class Animal {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    speak() {
+      console.log(`${this.name} makes a sound`);
+    }
+  }
+  
+  class Dog extends Animal {
+    constructor(name, breed) {
+      super(name);
+      this.breed = breed;
+    }
+  
+    speak() {
+      console.log(`${this.name} barks`);
+    }
+  }
+  
+  const dog = new Dog("Buddy", "Labrador");
+  dog.speak();
 
 
+  // Fetch API for HTTP Requests
+const apiUrl = "https://jsonplaceholder.typicode.com/posts/1";
 
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
+
+
+  // Local Storage
+localStorage.setItem("username", "john_doe");
+
+const savedUsername = localStorage.getItem("username");
+
+console.log("Saved Username:", savedUsername);
+
+
+  
+
+  
